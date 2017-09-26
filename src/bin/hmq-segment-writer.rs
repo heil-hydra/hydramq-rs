c72d60065187ae4d2ab7cf8d79e14da53e0e52fe
Jimmie Fulton <jimmie.fulton@gmail.com>
2017-09-26 09:36:47 -0700

    Write messages to segment

--- src/bin/hmq-segment-writer.rs
+++ src/bin/hmq-segment-writer.rs
@@ -1,4 +1,39 @@
+extern crate hydramq;
+
+use hydramq::message::{Message, Map, List};
+use hydramq::topic::{FileSegment, Segment};
 
 fn main() {
 
+    let mut segment = FileSegment::with_directory("example");
+    let message = example();
+    for _i in 0..1_000_000 {
+        segment.write(&message);
+    }
+}
+
+fn example() -> Message {
+    Message::new()
+        .with_property("fname", "Jimmie")
+        .with_property("lname", "Fulton")
+        .with_property("age", 42)
+        .with_property("temp", 98.6)
+        .with_property("vehicles", List::new()
+            .append("Aprilia")
+            .append("Infiniti")
+            .build()
+        )
+        .with_property("siblings",
+                       Map::new()
+                           .insert("brothers",
+                                   List::new()
+                                       .append("Jason").build()
+                           )
+                           .insert("sisters",
+                                   List::new()
+                                       .append("Laura")
+                                       .append("Sariah")
+                                       .build()
+                           ).build()
+        ).build()
 }
--- src/topic/mod.rs
+++ src/topic/mod.rs
@@ -1,5 +1,4 @@
-
-use std::fs::{self,OpenOptions, File};
+use std::fs::{self, OpenOptions, File};
 use std::path::{Path, PathBuf};
 use std::io::{self, Seek, SeekFrom, Write};
 
@@ -28,25 +27,24 @@ impl From<String> for SegmentNumber {
 }
 
 pub struct FileSegment {
+    directory: PathBuf,
     dat: File,
     idx: File,
+    size: u32,
 }
 
 pub trait Segment {
     fn write(&mut self, message: &Message);
+
+    fn size(&self) -> u32;
 }
 
 impl FileSegment {
+    pub fn with_directory<P>(directory: P) -> FileSegment where P: Into<PathBuf> {
+        let directory = directory.into();
+        fs::create_dir_all(directory.as_path()).expect("Error creating segment directory");
 
-    pub fn named(name: SegmentNumber) -> FileSegment {
-        let name: String = name.into();
-        Self::with_directory(PathBuf::from(name.as_str()))
-    }
-
-    pub fn with_directory(name: PathBuf) -> FileSegment {
-        fs::create_dir_all(name.as_path()).expect("Error creating segment directory");
-
-        let mut dat = name.clone();
+        let mut dat = directory.clone();
         dat.push("segment.dat");
         let dat = dat.as_path();
         let dat = OpenOptions::new()
@@ -56,20 +54,30 @@ impl FileSegment {
             .open(dat.clone())
             .expect(format!("Error creating {:?}", dat).as_str());
 
-        let mut idx = name.clone();
+        let mut idx = directory.clone();
         idx.push("segment.idx");
         let idx = idx.as_path();
-        let idx = OpenOptions::new()
+        let mut idx = OpenOptions::new()
             .append(true)
             .read(true)
             .create(true)
             .open(idx.clone())
             .expect(format!("Error creating {:?}", idx).as_str());
-        Self::new(dat, idx)
+        let file_size = idx.seek((SeekFrom::End(0))).unwrap() as u32;
+        eprintln!("file_size = {:?}", file_size);
+        let size = (idx.seek(SeekFrom::End(0)).unwrap() as u32) / 4;
+        eprintln!("size = {:?}", size);
+        FileSegment { directory, dat, idx, size}
     }
 
-    pub fn new(dat: File, idx: File) -> FileSegment {
-        FileSegment { dat, idx}
+    pub fn directory(&self) -> &Path {
+        self.directory.as_ref()
+    }
+
+    pub fn delete(self) -> io::Result<()> {
+        drop(self.dat);
+        drop(self.idx);
+        fs::remove_dir_all(self.directory)
     }
 }
 
@@ -80,48 +88,25 @@ impl Segment for FileSegment {
         let message_start = self.dat.seek(SeekFrom::End(0)).unwrap();
         encode_message(message, &mut contents);
         header.put_u32::<LittleEndian>(contents.len() as u32);
-        let mut header = header.freeze();
-        let mut contents = contents.freeze();
+        let header = header.freeze();
+        let contents = contents.freeze();
         self.dat.write_all(header.as_ref()).unwrap();
         self.dat.write_all(contents.as_ref()).unwrap();
         self.idx.seek(SeekFrom::End(0)).unwrap();
         let mut message_start_buffer = BytesMut::with_capacity(4);
         message_start_buffer.put_u32::<LittleEndian>(message_start as u32);
         self.idx.write_all(&mut message_start_buffer);
+        self.size += 1;
     }
 
+    fn size(&self) -> u32 {
+        self.size
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn create_segment() {
-        println!("Running test!");
-        let dat_path = "segment.dat";
-        let dat = OpenOptions::new()
-            .append(true)
-            .read(true)
-            .create(true)
-            .open(dat_path.clone()).expect("Error creating segment.dat");
-        let idx_path = "segment.idx";
-        let idx = OpenOptions::new()
-            .append(true)
-            .read(true)
-            .create(true)
-            .open(idx_path.clone()).expect("Error creating segment.idx");
-        let mut segment = FileSegment::new(dat, idx);
-
-        for i in 0..1000 {
-            let message = Message::new().with_body(format!("Hello{}\n", i)).build();
-            segment.write(&message);
-        }
-
-        drop(segment);
-        fs::remove_file(dat_path).expect("Error deleting segment.dat");
-        fs::remove_file(idx_path).expect("Error deleting segment.idx");
-    }
 
     #[test]
     fn create_new2() {
@@ -135,11 +120,66 @@ mod test {
     }
 
     #[test]
-    fn create_segment2() {
-        let mut segment = FileSegment::with_directory(PathBuf::from("segment"));
+    fn size_of_empty_segment() {
+        let segment = FileSegment::with_directory("segment2");
+        assert_eq!(segment.size(), 0);
+        segment.delete().unwrap();
+    }
+
+    #[test]
+    fn size_of_segment_with_messages() {
+        let mut segment = FileSegment::with_directory("segment3");
+        let message = Message::new().build();
+        &segment.write(&message);
+        assert_eq!(segment.size(), 1);
+        &segment.write(&message);
+        assert_eq!(segment.size(), 2);
+        segment.delete().unwrap();
+    }
+
+    #[test]
+    fn size_of_existng_segment() {
+        let mut segment = FileSegment::with_directory("segment4");
+        let message = Message::new().build();
+        &segment.write(&message);
+        assert_eq!(segment.size(), 1);
+        &segment.write(&message);
+        assert_eq!(segment.size(), 2);
+        drop(segment);
+        let mut segment = FileSegment::with_directory("segment4");
+        assert_eq!(segment.size(), 2);
+        segment.delete().unwrap();
+    }
+
+    #[test]
+    fn write_single_message() {
+        let mut segment = FileSegment::with_directory(PathBuf::from("segment5"));
         let message = Message::new()
             .with_body("Hello, World")
             .build();
         segment.write(&message);
+        drop(segment);
+        let mut dat = OpenOptions::new()
+            .read(true)
+            .create(false)
+            .open("segment/segment.dat").unwrap();
+        let seek = dat.seek(SeekFrom::Current(0)).unwrap();
+
+        let mut buffer = [0u8; 4];
+        use std::io::Read;
+        dat.read_exact(&mut buffer[..]).unwrap();
+        let mut bytes = ::bytes::Bytes::from(&buffer[..]).into_buf();
+
+        let message_size = &bytes.get_u32::<LittleEndian>();
+        let mut buffer: Vec<u8> = Vec::new();
+        dat.read_exact(&mut buffer[..]);
+        let mut bytes = buffer.into_buf();
+        let output = ::codec::decode_message(&mut bytes);
+        assert_eq!(message, output);
+        assert_eq!(message.body(), Some(&Value::from("Hello, World!")));
+        assert_eq!(message.properties().len(), 0);
+
+        let segment = FileSegment::with_directory(PathBuf::from("segment5"));
+        segment.delete();
     }
 }
